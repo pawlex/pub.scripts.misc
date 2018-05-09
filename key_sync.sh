ba502b5025a2ef463e7e08ccb18cdfbda51fc514
Paul Komurka <31913417+pawlex@users.noreply.github.com>
2018-05-09 10:46:23 -0700

    Update key_sync.sh
    + Comments

--- key_sync.sh
+++ key_sync.sh
@@ -5,11 +5,15 @@ CURL=/usr/bin/curl
 TARGET=~/.ssh/authorized_keys
 TEMP=$TARGET.new
 
+# if CURL is installed
 if [ -f $CURL ]; then
+ # Grab SSH Keys from github
  $($CURL -s https://github.com/$USERNAME.keys -o $TEMP)
 fi
 
+# If temp file exists and is not null
 if [ -s $TARGET.new ]; then
+ # Overwrite authorized_keys with that on github
  mv -f $TEMP $TARGET
  chmod 600 $TARGET
 fi
