4650349580cc0b9efaf3b7a12a49c5146d1fa74a
Paul Komurka <31913417+pawlex@users.noreply.github.com>
2018-05-09 10:31:03 -0700

    Create key_sync.sh
    Keeps SSH authorized_key file up to date

---
+++ key_sync.sh
@@ -0,0 +1,17 @@
+!/bin/bash
+
+USERNAME=pawlex
+CURL=/usr/bin/curl
+TARGET=~/.ssh/authorized_keys
+TEMP=$TARGET.new
+
+if [ -f $CURL ]; then
+ $($CURL -s https://github.com/$USERNAME.keys -o $TEMP)
+fi
+
+if [ -s $TARGET.new ]; then
+ mv -f $TEMP $TARGET
+ chmod 600 $TARGET
+fi
+
+exit 0
