5295f6bf527be29a6caf92a721ce251669ed356e
Paul Komurka <pawlex@gmail.com>
2018-12-16 01:00:53 -0500

    check for curl

--- key_sync.sh
+++ key_sync.sh
@@ -5,10 +5,18 @@ CURL=/usr/bin/curl
 TARGET=~/.ssh/authorized_keys
 TEMP=$TARGET.new
 
+if [ ! -d ~/.ssh ]; then
+  mkdir -p ~/.ssh
+  chmod 700 ~/.ssh
+fi
+
 # if CURL is installed
 if [ -f $CURL ]; then
- # Grab SSH Keys from github
- $($CURL -s https://github.com/$USERNAME.keys -o $TEMP)
+  # Grab SSH Keys from github
+  $($CURL -s https://github.com/$USERNAME.keys -o $TEMP)
+ else
+  echo "key not updated.  curl is not installed"
+  exit -1
 fi
 
 # If temp file exists and is not null
